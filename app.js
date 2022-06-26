7b13b0ae8591520b81879d2623e5349b30d9024e
Wondrushh <34789256+Wondrushh@users.noreply.github.com>
2022-06-26 17:53:44 +0200

    Enabled uploading to FTP server

--- app.js
+++ app.js
@@ -8,13 +8,18 @@ let fs = require("fs");
 
 // Start server
 http.createServer(function (req, res){
+    // Start uploading files on submit button click
     if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
+
+        // Move the file from temporary folder to root folder
         form.parse(req, function (err, fields, files) {
             var oldpath = files.filetoupload.filepath;
             var newpath = __dirname + "/" + files.filetoupload.originalFilename;
+            
             fs.rename(oldpath, newpath, function (err) {
                 if (err) throw err;
+                uploadFilesToFTP(files.filetoupload.originalFilename);
                 res.write('Soubor je uspesne na serveru!');
                 res.end();
             });
@@ -24,38 +29,42 @@ http.createServer(function (req, res){
         var q = url.parse(req.url, true);
         // Get name of accessed file
         var filename = "." + q.pathname;
-        // Read the accessed file
+        // Read the accessed file 
         fs.readFile(filename, function(err, data){
         if (err) {
             res.writeHead(404, {'Content-Type': 'text/html'});
             return res.end("404 Not Found");
             } 
+
         res.writeHead(200, {'Content-Type': 'text/html'});
+        // Display the read file (html)
         res.write(data);
         return res.end();
     })}
 }).listen(8080);
 
 
-// example();
+// Function for uploading a file from server root to FTP server specified by IP
+async function uploadFilesToFTP(fileName) {
+    const client = new ftp.Client()
+    client.ftp.verbose = false;
 
-// async function example() {
-//     const client = new ftp.Client()
-//     client.ftp.verbose = true
-//     try {
-//         await client.access({
-//             host: "192.168.12.11",
-//             port: 2221,
-//             user: "android",
-//             password: "1234",
-//             secure: false,
-//         })
-//         console.log(await client.list())
-//         await client.uploadFrom("README.md", "README_FTP_TLS.md")
-//         await client.downloadTo("README_COPY_TLS.md", "README_FTP_TLS.md")
-//     }
-//     catch(err) {
-//         console.log(err)
-//     }
-//     client.close()
-// }
+    try {
+        // Access the server
+        // !!! WRITE THE CORRECT IP ADDRESS HERE !!!
+        await client.access({
+            host: "192.168.199.211",
+            port: 2221,
+            user: "android",
+            password: "1234",
+            secure: false,
+        })
+        // Upload the file to FTP server
+        await client.uploadFrom(fileName, fileName);
+    }
+    
+    catch(err) {
+        console.log(err);
+    }
+    client.close();
+}
